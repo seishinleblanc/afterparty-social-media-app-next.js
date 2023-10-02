8124c0d13f400774566b1693a7ba4c21fee707c6
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-10-01 20:18:06 -0500

    Finished home page design.

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,5 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,6 +10,32 @@ function Topbar() {
             <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
             <p className="text-heading3-bold text-light-1 max-:hidden">AfterParty</p>
         </Link>
+
+        <div className="flex items-center gap-1">
+            <div className="block md:hidden">
+                <SignedIn>
+                    <SignOutButton>
+                        <div className="flex cursor-pointer"> 
+                            <Image
+                            src="/assets/logout.svg"
+                            alt="logout"
+                            width={24}
+                            height={24}
+                            />
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
+            </div>
+            <OrganizationSwitcher 
+                appearance={{
+                    baseTheme: dark,
+                    elements: {
+                        organizationSwitcherTrigger:
+                        "py-2 px-4"
+                    }
+                }}
+            />
+        </div>
     </nav>
     )
 }
